1b5f90c00ed1a4f2270dcab19509837ff089f71e
vince2bir <vincent@debier.re>
2020-08-29 15:03:08 +0200

    Update translations.php

--- Locale/fr_FR/translations.php
+++ Locale/fr_FR/translations.php
@@ -2,12 +2,12 @@
 
 return array(
 		'Assigned Group' => 'Groupe assigné',
-    'Assigned Group:' => 'Groupe assigné:',
-    'Other Assignees' => 'Autres assignations',
-    'Other Assignees:' => 'Autres assignations:',
-    'Send a task by email to assigned group members' => 'Envoyer une tâche par courriel aux membres du groupe assigné',
+		'Assigned Group:' => 'Groupe assigné:',
+		'Other Assignees' => 'Autres assignations',
+		'Other Assignees:' => 'Autres assignations:',
+		'Send a task by email to assigned group members' => 'Envoyer une tâche par courriel aux membres du groupe assigné',
 		'Send a task by email to the other assignees for a task.' => 'Envoyer une tâche par courriel aux autres personnes assignées à cette tâche',
 		'Send email notification of impending due date to Group Members Assigned' => 'Envoyer une notification de date d\'échéance imminente par courriel aux membres du groupe assigné',
 		'Send email notification of impending due date to the other assignees of a task' => 'Envoyer une notification de date d\'échéance imminente par courriel aux autres personnes assignés à cette tâche',
-    'Assign the task to a specific group' => 'Assigner cette tâche à un groupe spécifique'
+		'Assign the task to a specific group' => 'Assigner cette tâche à un groupe spécifique'
 );
